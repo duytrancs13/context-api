9c28a315285f05c3c09366a64c3256a01d52fc71
Admin mini <adminmini@front-end3forcoms-Mac-mini.local>
2019-10-28 15:00:06 +0700

    manager state in reactjs by contextAPI.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const Green = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="green">
+      <MyContext.Consumer>
+        {context => (
+          <div>
+            <p> {context.state.number}</p>
+            <button onClick={context.countUp}>Click</button>
+          </div>
+        )}
+      </MyContext.Consumer>
     </div>
-  );
+  )
+}
+
+const Blue = (props) => {
+  return (
+    <div className="blue">
+      <Green />
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-undef
+class Red extends Component {
+  render () {
+    return (
+      <div className="red">
+        <MyContext.Consumer>
+          {(context) => context.state.number}
+        </MyContext.Consumer >
+        <Blue />
+      </div>
+    )
+  }
+}
+
+const MyContext = React.createContext();
+// eslint-disable-next-line no-undef
+class MyProvider extends Component {
+  state = {
+    number: 10
+  }
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        countUp: () => this.setState({
+          number: this.state.number + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+// eslint-disable-next-line no-undef
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <h1>Demo context API</h1>
+        <Red />
+      </MyProvider>
+    )
+  }
 }
 
 export default App;
